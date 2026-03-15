8cfde1d27170d66ae505e1d2bcd791f433726cf5
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-03-15 13:37:15 -0300

    Implementação da criação e validação de tarefas no quadro Kanban

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputTarefa = document.getElementById("input-tarefa");
+
+const btnCriar = document.getElementById("btn-criar");
+
+const aFazer = document.getElementById("a-fazer");
+
+btnCriar.addEventListener("click", criarTarefa);
+
+
+
+function criarTarefa() {
+
+    const textoTarefa = inputTarefa.value.trim();
+
+    console.log(textoTarefa);
+
+    if(textoTarefa === "") {
+        alert("Digite uma Tarefa ")
+        return
+    }
+
+    const novoCard = document.createElement("div");
+
+    novoCard.classList.add("card")
+
+    novoCard.textContent = textoTarefa
+
+    aFazer.appendChild(novoCard)
+
+}
